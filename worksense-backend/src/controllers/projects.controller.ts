7e5f6d9d7ab99e0618f9aa1f20e00b22bd857fb7
hugoOchoaTec <hugo_ochoa@tec.mx>
2025-04-01 10:09:23 -0600

    Tres rutas project

---
+++ worksense-backend/src/controllers/projects.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { db } from "../models/firebase.js";
+
+// Obtener todos los proyectos
+export const getProjects = async (req: Request, res: Response) => {
+  try {
+    const projects = await db.collection("projects").get();
+    const list: any[] = [];
+    projects.forEach((doc) => {
+      list.push({ id: doc.id, ...doc.data() });
+    });
+    res.json(list);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener proyectos" });
+  }
+};
+
+// Obtener un proyecto por ID
+export const getProjectById = async (req: Request, res: Response) => {
+  try {
+    const projectDoc = await db.collection("projects").doc(req.params.id).get();
+
+    if (!projectDoc.exists) {
+      return res.status(404).json({ error: "Proyecto no encontrado" });
+    }
+
+    res.json({ id: req.params.id, ...projectDoc.data() });
+  } catch (error) {
+    console.error("Error al obtener el proyecto:", error);
+    res.status(500).json({ error: "Error al obtener el proyecto" });
+  }
+};
+
+// Crear un nuevo proyecto
+export const createProject = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+    const newProject = await db.collection("projects").add({ name });
+    res.status(201).json({ id: newProject.id, name });
+  } catch (error) {
+    res.status(500).json({ error: "Error al crear el proyecto" });
+  }
+};
