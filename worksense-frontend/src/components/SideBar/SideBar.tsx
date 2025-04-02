f66a43850e93f178a3a5cf54ff3373318dd82255
Santiagoschzr <151122664+Santiagoschzr@users.noreply.github.com>
2025-04-02 16:45:29 -0600

    "Logo update on main"

--- worksense-frontend/src/components/SideBar/SideBar.tsx
+++ worksense-frontend/src/components/SideBar/SideBar.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import styles from './SideBar.module.css';
+import React from "react";
+import styles from "./SideBar.module.css";
+import worksenseLogo from "@/assets/images/worksenseLogo.svg";
 
 export const SideBar: React.FC = () => {
   return (
     <aside className={styles.sidebar}>
       <div className={styles.logo}>
-        <img src="/WorkSenseLogo.svg" alt="WorkSense Logo" />
+        <img src={worksenseLogo} alt="WorkSense Logo" />
       </div>
 
       <nav>
         <ul>
           <li className={styles.active}>
-            <span className={styles.icon}><img src="/Book open.svg" alt="Projects" /></span> My Projects
+            <span className={styles.icon}>
+              <img src="/Book open.svg" alt="Projects" />
+            </span>{" "}
+            My Projects
           </li>
           <li>
-            <span className={styles.icon}><img src="/Home.svg" alt="Account" /></span> Account
+            <span className={styles.icon}>
+              <img src="/Home.svg" alt="Account" />
+            </span>{" "}
+            Account
           </li>
         </ul>
       </nav>
     </aside>
   );
-}; 
+};
