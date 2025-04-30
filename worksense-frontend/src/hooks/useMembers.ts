05dfb1896814e0621bbb72a005bfb3852dda46fc
Jorge Andrés Palacios Plascencia <jorgepalaciospg@outlook.com>
2025-04-29 19:08:38 -0600

    Added useDeleteMembers in useMembershook,.

--- worksense-frontend/src/hooks/useMembers.ts
+++ worksense-frontend/src/hooks/useMembers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { projectService } from '@/services/projectService';
 import MemberDetailed from '@/types/MemberDetailedType';
 
@@ -7,5 +7,17 @@ export function useMembers(projectId: string) {
     queryKey: ['members', projectId],
     queryFn: () => projectService.fetchProjectMembersDetailed(projectId),
     enabled: !!projectId, // Only fetch if projectId exists
+  });
+}
+
+export function useDeleteMember(projectId: string) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (memberId: number) => projectService.removeMemberFromProject(projectId, memberId),
+    onSuccess: () => {
+      // Invalidate or update the cache after deletion
+      queryClient.invalidateQueries({ queryKey: ['members', projectId] });
+    },
   });
 }
