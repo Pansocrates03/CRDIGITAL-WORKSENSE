496ea10b3b088550a781451f0c1a083550c80432
Luis Gonzalez <114023856+LuisGzz13@users.noreply.github.com>
2025-05-21 17:05:56 -0600

    merge conlicts fix

--- worksense-frontend/src/App.tsx
+++ worksense-frontend/src/App.tsx
@@ -20,7 +20,7 @@ import SprintPage from "./pages/Sprints/SprintPage";
 import WorkflowPage from "./pages/Workflow/WorkflowPage";
 import ProjectSettingsPage from "./pages/ProjectSettings/ProjectSettingsPage";
 import { Toaster } from "sonner";
-
+import ForYouPage from "./pages/ForYouPage.tsx";
 const queryClient = new QueryClient();
 
 console.log("Running");
