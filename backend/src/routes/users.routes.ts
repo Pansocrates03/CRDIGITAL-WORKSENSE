fda1efb5e00b09f1298c8fb67babba22bc4b8591
Santiagoschzr <151122664+Santiagoschzr@users.noreply.github.com>
2025-05-27 20:35:06 -0600

    changes to fix create User

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -6,7 +6,7 @@ const router = express.Router({ mergeParams: true });
 router.get("/users", getUsers);
 router.get("/user/:userId", getUser);
 router.put("/user/:userId", updateUser);
-router.post("/user", createUser);
+router.post("/users", createUser);
 router.delete("/user/:userId", deleteUser)
 
 export default router;
